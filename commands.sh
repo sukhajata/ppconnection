b429cd408262b6bedc39052d01e2fb46f0139ea8
Tim Cranfield <tcranfield@electronet.co.nz>
2020-07-16 11:27:35 +1200

    Removed streaming

--- commands.sh
+++ commands.sh
@@ -10,5 +10,9 @@ protoc -I=. *.proto \
   --js_out=import_style=commonjs:web \
   --grpc-web_out=import_style=commonjs,mode=grpcwebtext:web
 
+#binary for streaming
+protoc -I=. *.proto \
+  --js_out=import_style=commonjs,binary:web_b
+
 #python
 python -m grpc_tools.protoc -I . --python_out=python --grpc_python_out=python *.proto
